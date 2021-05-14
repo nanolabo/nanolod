93ec947967fbdb3cb9cda2762cff4410ccef55c9
Olivier Giniaux <oginiaux@gmail.com>
2021-05-14 20:56:08 +0200

    Remove useless nanolod menu

--- Nanolod.Editor/MainMenu.cs
+++ Nanolod.Editor/MainMenu.cs
@@ -5,10 +5,6 @@ namespace Nanolod
 {
     public static class MainMenu
     {
-        [MenuItem("Nanolod/Test")]
-        public static void Test()
-        {
-            Debug.Log("Hello !");
-        }
+
     }
 }
